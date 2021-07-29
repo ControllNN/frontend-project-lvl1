40574e6e98002d68240e874400a679c63bff5d57
Alexey G <1@lala.com>
2021-07-29 21:42:36 +0200

    refactoring to make reusable

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,15 @@
 import readlineSync from "readline-sync";
 
-export const printName = () => {
-  const name = readlineSync.question("May I have your name? ");
+export const greet = () => {
+  console.log("Welcome to the Brain Games!");
+};
 
+let name;
+export const printName = () => {
+  name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
+};
+
+export const getName = () => {
+  return name;
 };
